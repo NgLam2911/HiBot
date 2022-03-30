3ae83adf13a43214e38e5519e71eeb51fc2c67eb
Ngọc Lam <33188123+NgLamVN@users.noreply.github.com>
2022-03-30 09:44:43 +0700

    Update bot_constant.py

--- bot/bot_constant.py
+++ bot/bot_constant.py
@@ -1,6 +1,6 @@
 class bot_constant:
     bot_version: str = "0.0.1"
-    bot_token: str = "OTM0OTg2MTAzMDEyODc2MzU5.Ye4Dqg.HU3d1Trd0HKN5feS9VivLm94cyY"
+    bot_token: str = "<Replace token in here>"
     bot_author: str = "NgLam2911"
     bot_prefix: str = "?"
     bot_description: str = "Just a very simple discord bot that say Hi if you say Hello..."
